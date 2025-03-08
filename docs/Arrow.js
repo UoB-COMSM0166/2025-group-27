35a770fc7f5de974c3c5fbfb862034ed56172b36
realYDIAN <alchemistbackydian@gmail.com>
2025-03-08 14:51:48 +0000

    add archer upgrade option

--- docs/Arrow.js
+++ docs/Arrow.js
@@ -1,6 +1,6 @@
 // ===== 新增Arrow类 =====
 class Arrow {
-    constructor(x, y, direction, speed, damage, size, img, state) {
+    constructor(x, y, direction, speed, damage, size, img, state, canPierce = false, canSplit = false) {
       this.pos = createVector(x, y);
       this.vel = direction.copy().mult(speed);
       this.damage = damage;
@@ -11,6 +11,11 @@ class Arrow {
       this.imgWidth = size * 2;
       this.imgHeight = size;
       this.state = state;
+
+      this.canPierce = canPierce; //穿透
+      this.canSplit = canSplit; //散射
+      this.hasSplit = false; //是否已经散射
+      this.hitEnemies = new Set();
     }
   
     update() {
@@ -53,5 +58,32 @@ class Arrow {
           );
         }
         pop();
+    }
+
+    handleCollision(enemy) {
+      if (this.canPierce) {
+        if (!this.hitEnemies.has(enemy)) { // **避免重复伤害**
+          this.hitEnemies.add(enemy);
+          return true; // 造成伤害
+        }
+        // 穿透逻辑，不销毁箭矢
+        return false;
+      } else {
+        // 普通箭矢，碰撞后销毁
+        this.isActive = false;
+        return true;
+      }
+    }
+
+    split() {
+      if (this.canSplit && !this.hasSplit) {
+        this.hasSplit = true;
+        // 创建两个新的箭矢，方向稍微偏移
+        let angleOffset = PI / 12; // 15度偏移
+        let dir1 = this.vel.copy().rotate(-angleOffset);
+        let dir2 = this.vel.copy().rotate(angleOffset);
+        player.arrows.push(new Arrow(this.pos.x, this.pos.y, dir1, this.vel.mag()*0.1, this.damage, this.size, this.img, this.state, this.canPierce, false));
+        player.arrows.push(new Arrow(this.pos.x, this.pos.y, dir2, this.vel.mag()*0.1, this.damage, this.size, this.img, this.state, this.canPierce, false));
+      }
     }
   }
