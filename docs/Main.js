842dd9480b521d9db8c1dc3d32cde413f4c0970d
chrisheng456 <1348797564@qq.com>
2025-03-08 17:27:28 +0000

    debug-add-sun

--- docs/Main.js
+++ docs/Main.js
@@ -181,7 +181,7 @@ function preload() {
   // weather
   snowflakepic = loadImage("./assets/selected_images/weather/flake.gif");
   lightningpic = loadImage("./assets/selected_images/weather/lightning.gif");
-  sunpic = loadImage("./assets/selected_images/weather/sun_yellow.gif");
+  sunpic = loadImage("./assets/selected_images/weather/sun_yellow.png");
 
   // knight
   KnightActionUp = loadImage("./assets/selected_images/characters/keyboardman/keyboardman_back.png");
