93bbb9ff262584cd86ee668c8ecb00c0ac5c0a21
Feihang027 <vj24070@bristol.ac.uk>
2025-05-10 20:34:05 +0100

    Obstacle UI comments

--- docs/Obstacle.js
+++ docs/Obstacle.js
@@ -1,27 +1,32 @@
 class Obstacle {
   constructor(x, y, width, height, isVertical, isSpecial = false) {
     this.isSpecial = isSpecial;
-    if(!isSpecial){
-    this.pos = createVector(x, y);
-    this.width = width;
-    this.height = height;
-  } else {
-    this.pos = createVector(431,200);
-    this.width = 200;
-    this.height = 200;
-  }
+    if (!isSpecial) {
+      // Standard obstacle: use provided position and dimensions
+      this.pos = createVector(x, y);
+      this.width = width;
+      this.height = height;
+    } else {
+      // Special obstacle: always at fixed coordinates, fixed size
+      this.pos = createVector(431, 200);
+      this.width = 200;
+      this.height = 200;
+    }
+    // Orientation flag (affects which sprite to draw)
     this.isVertical = isVertical;
+    // Movement state
     this.moving = false;
     this.targetPos = null;
     this.moveStartTime = 0;
     this.moveStartPos = null;
   }
-
+  // Handle smooth easing movement toward targetPos over 2 seconds
   update() {
     if (this.moving && this.targetPos) {
       let elapsed = millis() - this.moveStartTime;
       const duration = 2000;
       if (elapsed < duration) {
+        // Ease in/out quadratic timing
         let progress = elapsed / duration;
         progress =
           progress < 0.5
@@ -30,13 +35,14 @@ class Obstacle {
         this.pos.x = lerp(this.moveStartPos.x, this.targetPos.x, progress);
         this.pos.y = lerp(this.moveStartPos.y, this.targetPos.y, progress);
       } else {
+        // Movement complete
         this.pos = this.targetPos.copy();
         this.moving = false;
         this.targetPos = null;
       }
     }
   }
-
+  // Begin moving the obstacle to newPos
   moveTo(newPos) {
     this.moving = true;
     this.targetPos = newPos.copy();
@@ -44,39 +50,43 @@ class Obstacle {
     this.moveStartTime = millis();
   }
 
+  // Draw the obstacle using different sprites depending on wave and orientation
   display() {
-    if(this.isSpecial === true){
+    if (this.isSpecial === true) {
+      // Always draw the special obstacle sprite
       image(obstacleS, 431, 200, 200, 200);
     } else {
-    if(wave <= 5){
-    if(this.isVertical){
-      image(obstacle1, this.pos.x, this.pos.y, this.width, this.height);
-    } else{
-      image(obstacle2, this.pos.x, this.pos.y, this.width, this.height);
-    }
-    } else if(wave>5 && wave<=10){
-      if(this.isVertical){
-        image(obstacle3, this.pos.x, this.pos.y, this.width, this.height);
-      } else{
-        image(obstacle, this.pos.x, this.pos.y, this.width, this.height);
+      // Choose sprite based on current wave number
+      if (wave <= 5) {
+        if (this.isVertical) {
+          image(obstacle1, this.pos.x, this.pos.y, this.width, this.height);
+        } else {
+          image(obstacle2, this.pos.x, this.pos.y, this.width, this.height);
+        }
+      } else if (wave > 5 && wave <= 10) {
+        if (this.isVertical) {
+          image(obstacle3, this.pos.x, this.pos.y, this.width, this.height);
+        } else {
+          image(obstacle, this.pos.x, this.pos.y, this.width, this.height);
+        }
+
+      } else if (wave > 10 && wave <= 15) {
+        if (this.isVertical) {
+          image(obstacle5, this.pos.x - 2, this.pos.y - 2, 100, 100);
+        } else {
+          image(obstacle7, this.pos.x - 2, this.pos.y - 2, 70, 70);
+        }
+      } else {
+        if (this.isVertical) {
+          image(obstacle9, this.pos.x, this.pos.y, 70, 70);
+        } else {
+          image(obstacleV, this.pos.x, this.pos.y, 100, 50);
+        }
       }
-      
-    } else if(wave>10 && wave<=15){
-      if(this.isVertical){
-        image(obstacle5, this.pos.x-2, this.pos.y-2, 100, 100);
-        } else {
-        image(obstacle7, this.pos.x-2, this.pos.y-2, 70, 70);
-        }
-    } else {
-      if(this.isVertical){
-        image(obstacle9, this.pos.x, this.pos.y, 70, 70);
-        } else {
-        image(obstacleV, this.pos.x, this.pos.y, 100, 50);
-        }
     }
-  }
   }
 
+  // Axis-aligned bounding-box collision test against another rectangle
   collidesWith(otherPos, otherWidth, otherHeight) {
     return !(
       otherPos.x + otherWidth < this.pos.x ||
@@ -86,7 +96,9 @@ class Obstacle {
     );
   }
 
+  // Collision test against a circle of given center and radius
   collidesWithCircle(position, radius) {
+    // Find the closest point on the rectangle to the circle center
     let closestX = constrain(position.x, this.pos.x, this.pos.x + this.width);
     let closestY = constrain(position.y, this.pos.y, this.pos.y + this.height);
     let distanceSquared = sq(position.x - closestX) + sq(position.y - closestY);
--- docs/ui.js
+++ docs/ui.js
@@ -1,57 +1,60 @@
 //------------------Class Button----------------------
 class Button {
-    constructor(x, y, w, h, label, action) {
-      this.x = x;
-      this.y = y;
-      this.w = w;
-      this.h = h;
-      this.label = label;
-      this.action = action;
-    }
-  
-    display() {
-      push();
-      fill(100);
-      stroke(200);
-      rectMode(CORNER);
-      rect(this.x, this.y, this.w, this.h, 5);
-      fill(255);
-      noStroke();
-      textAlign(CENTER, CENTER);
-      textSize(16);
-      text(this.label, this.x + this.w / 2, this.y + this.h / 2);
-      pop();
-    }
-  
-    contains(mx, my) {
-      return (
-        mx > this.x && mx < this.x + this.w && my > this.y && my < this.y + this.h
-      );
-    }
+  constructor(x, y, w, h, label, action) {
+    this.x = x;
+    this.y = y;
+    this.w = w;
+    this.h = h;
+    this.label = label;
+    this.action = action;
   }
-  
-//----------------------Class FloatingText------------------
+
+  // Draw the button rectangle, border, and centered text
+  display() {
+    push();
+    fill(100);
+    stroke(200);
+    rectMode(CORNER);
+    rect(this.x, this.y, this.w, this.h, 5);
+    fill(255);
+    noStroke();
+    textAlign(CENTER, CENTER);
+    textSize(16);
+    text(this.label, this.x + this.w / 2, this.y + this.h / 2);
+    pop();
+  }
+
+  // Check if a point (mouseX, mouseY) is inside the button
+  contains(mx, my) {
+    return (
+      mx > this.x && mx < this.x + this.w && my > this.y && my < this.y + this.h
+    );
+  }
+}
+
+//Class FloatingText
 class FloatingText {
-    constructor(text, x, y, col) {
-      this.text = text;
-      this.pos = createVector(x, y);
-      this.col = col;
-      this.alpha = 255;
-      this.lifetime = 60;
-    }
-  
-    update() {
-      this.pos.y -= 1;
-      this.alpha -= 255 / this.lifetime;
-      return this.alpha > 0;
-    }
-  
-    display() {
-      push();
-      textAlign(CENTER);
-      textSize(16);
-      fill(red(this.col), green(this.col), blue(this.col), this.alpha);
-      text(this.text, this.pos.x, this.pos.y);
-      pop();
-    }
+  constructor(text, x, y, col) {
+    this.text = text;
+    this.pos = createVector(x, y);
+    this.col = col;
+    this.alpha = 255;
+    this.lifetime = 60;
   }
+
+  update() {
+    this.pos.y -= 1;
+    this.alpha -= 255 / this.lifetime;
+    return this.alpha > 0;
+  }
+
+  // Draw the text with its current alpha
+  display() {
+    push();
+    textAlign(CENTER);
+    textSize(16);
+    fill(red(this.col), green(this.col), blue(this.col), this.alpha);
+    text(this.text, this.pos.x, this.pos.y);
+    pop();
+  }
+}
